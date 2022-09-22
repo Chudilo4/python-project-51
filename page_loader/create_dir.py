b825156483502b9ad2078c243f6e79981e6ce8c8
Артём <chipalinchik@gmail.com>
2022-09-22 22:19:01 +0500

    fix step four

--- page_loader/create_dir.py
+++ page_loader/create_dir.py
@@ -4,18 +4,20 @@
 import os
 import shutil
 import logging
-import sys
 
 
 def create_dir(path):
-    logging.basicConfig(level='CRITICAL')
     logger = logging.getLogger()
+    logging.basicConfig(level='CRITICAL')
     path2 = os.path.join(path, '_files')
     try:
         os.mkdir(path2)
+    except PermissionError as err:
+        logger.error("Permission error: {}".format(err))
+        raise SystemExit(err) from None
     except FileNotFoundError as err:
         logger.critical('not found file', err)
-        sys.exit()
+        raise SystemExit(err) from None
     except FileExistsError:
         shutil.rmtree(path2)
         os.mkdir(path2)
