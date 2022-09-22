d5f441f878d1d457b6042a7716dd38453ed16467
Артём <chipalinchik@gmail.com>
2022-09-22 20:24:07 +0500

    fix step four

--- page_loader/create_dir.py
+++ page_loader/create_dir.py
@@ -13,10 +13,10 @@ def create_dir(path):
     path2 = os.path.join(path, '_files')
     try:
         os.mkdir(path2)
-    except FileExistsError:
-        shutil.rmtree(path2)
-        os.mkdir(path2)
     except FileNotFoundError:
         logger.info('not found file')
         sys.exit()
+    except FileExistsError:
+        shutil.rmtree(path2)
+        os.mkdir(path2)
     return path2
