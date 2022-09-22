ef385e03e5b693adc1ecb22862470c93f34288f4
Артём <chipalinchik@gmail.com>
2022-09-23 01:35:38 +0500

    fix step two

--- page_loader/create_dir.py
+++ page_loader/create_dir.py
@@ -1,9 +1,10 @@
 # file <create_dir.py>
 
 
+import logging
 import os
-import shutil
 import re
+import sys
 
 
 def create_dir(path, url):
@@ -12,7 +13,7 @@ def create_dir(path, url):
     path2 = os.path.join(path, format_url[2:])
     try:
         os.mkdir(path2)
-    except FileExistsError:
-        shutil.rmtree(path2)
-        os.mkdir(path2)
+    except Exception as err:
+        logging.CRITICAL(err)
+        sys.exit(err)
     return path2
