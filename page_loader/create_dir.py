75cf45d26f10fc002ec5728e06642adcf665e1f4
Артём <chipalinchik@gmail.com>
2022-09-22 20:20:54 +0500

    fix step four

--- page_loader/create_dir.py
+++ page_loader/create_dir.py
@@ -3,9 +3,13 @@
 
 import os
 import shutil
+import logging
+import sys
 
 
 def create_dir(path):
+    logging.basicConfig(level='INFO')
+    logger = logging.getLogger()
     path2 = os.path.join(path, '_files')
     try:
         os.mkdir(path2)
@@ -13,5 +17,6 @@ def create_dir(path):
         shutil.rmtree(path2)
         os.mkdir(path2)
     except FileNotFoundError:
-        raise Warning('file not found')
+        logger.info('not found file')
+        sys.exit()
     return path2
