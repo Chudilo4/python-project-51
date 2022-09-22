765332112a172f9aa641c88f676c28c49979486b
Артём <chipalinchik@gmail.com>
2022-09-22 23:54:00 +0500

    fix step two

--- page_loader/create_dir.py
+++ page_loader/create_dir.py
@@ -3,11 +3,13 @@
 
 import os
 import shutil
+import re
 
 
-def create_dir(path, name):
-    a = name + '_files'
-    path2 = os.path.join(path, a)
+def create_dir(path, url):
+    r = re.split(r'/\/|http:|https:|\.|\/', url)
+    format_url = '_'.join(r) + '_files'
+    path2 = os.path.join(path, format_url)
     try:
         os.mkdir(path2)
     except FileExistsError:
