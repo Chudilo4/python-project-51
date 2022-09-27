2bdb5d8b81eeaaed3a1f2b4f21abaa55447d6c66
Артём <chipalinchik@gmail.com>
2022-09-27 15:02:17 +0500

    fix step three

--- page_loader/format_file.py
+++ page_loader/format_file.py
@@ -5,10 +5,11 @@ import re
 
 
 def format_files(url):
-    exc = ['.com', '.ru', '.en']
-    r = re.split(r'/\/|http:|https:|\.|\/', url)
-    r2 = re.findall(r'\.\D{2,3}$', url)
+    r = re.split(r'/\/|https://|http://|\/|\.', url)
+    r2 = re.findall(r'\.\w{2,3}\b', url)
     format_url = '-'.join(r)
-    if not r2 or r2[0] not in exc:
-        return format_url[2:-4] + ".html"
-    return format_url[2:-4] + r2[0]
+    exc = ['.com', '.ru', '.en']
+    if r2[-1] in exc:
+        return format_url[1:] + ".html"
+    else:
+        return format_url[1:-4] + r2[-1]
