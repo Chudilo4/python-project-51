5d9f403425a7ee04a6b2d5ee0acee024d2a51323
Артём <chipalinchik@gmail.com>
2022-09-28 22:26:10 +0500

    fix

--- page_loader/script/main.py
+++ page_loader/script/main.py
@@ -4,25 +4,16 @@
 from page_loader.pars import parser
 from page_loader.page_loader import download
 import sys
-import logging
-import errno
 
 
 def main():
-    logger = logging.getLogger()
 
     try:
         url, o = parser()
         print(download(url, o))
-    except PermissionError as error:
-        logger.error(error)
-        sys.exit(errno.EPERM)
-    except FileNotFoundError as error:
-        logger.error(error)
-        sys.exit(errno.ENOENT)
-    except ConnectionError as error:
-        logger.error(error)
-        sys.exit(errno.ECONNREFUSED)
+    except Exception as e:
+        sys.exit(e)
+
 
 
 if __name__ == '__main__':
