fe1d1a477704b0276fa75536d2af53d4090dec94
Артём <chipalinchik@gmail.com>
2022-09-28 22:35:23 +0500

    fix

--- page_loader/script/main.py
+++ page_loader/script/main.py
@@ -11,9 +11,10 @@ def main():
     try:
         url, o = parser()
         print(download(url, o))
-        sys.exit(1)
     except Exception:
         sys.exit(0)
+    finally:
+        sys.exit(1)
 
 
 if __name__ == '__main__':
