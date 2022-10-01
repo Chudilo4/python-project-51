d7c9f7aa1d051cfa1fd8c1388726c3e76ff0767d
Артём <chipalinchik@gmail.com>
2022-10-01 21:26:02 +0500

    fix sys

--- page_loader/script/main.py
+++ page_loader/script/main.py
@@ -4,19 +4,15 @@
 from page_loader.pars import parser
 from page_loader.page_loader import download
 import sys
-import logging
 
 
 def main():
-    logging.basicConfig(level=logging.DEBUG)
-    logger = logging.getLogger()
     try:
         url, o = parser()
         print(download(url, o))
     except SystemExit as e:
-        logger.error(str(e))
-        sys.exit(e)
-    sys.exit(0)
+        sys.exit(str(e))
+    sys.exit(str(0))
 
 
 if __name__ == '__main__':
