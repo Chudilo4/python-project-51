6af0aa7040822af9ff346629dee9e8da656a448e
Артём <chipalinchik@gmail.com>
2022-10-12 23:51:36 +0500

    fix

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,6 +1,5 @@
 # file <test_download.py>
 import os
-import subprocess
 import tempfile
 import requests_mock
 
@@ -22,9 +21,6 @@ def test_download():
             r = download('https://site.com/blog/about', rood)
             excepted = open('tests/fixtures/expected/site-com-blog-about.html').read()
             w = open(r, 'r').read()
-            proc = subprocess.run('page-loader https://dota2.ru', shell=True)
-            df = proc.returncode
             assert len(os.listdir(os.path.join(rood, 'site-com-blog-about_files'))) == 4
             assert len(os.listdir(rood)) == 2
             assert excepted == w
-            assert df == 0
